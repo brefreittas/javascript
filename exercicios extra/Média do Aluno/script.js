ba29db17e25816f1d744fb81b09a4e36bc488091
Breno de Freitas <50001019+brefreittas@users.noreply.github.com>
2022-09-22 21:20:31 -0300

    formatei os codigos

--- exercicios extra/Média do Aluno/script.js
+++ exercicios extra/Média do Aluno/script.js
@@ -1,18 +1,19 @@
 function calcular() {
-    let nome = document.getElementById('txtnome').value
-    let nota1 = parseFloat(document.getElementById('nota1').value)
-    let nota2 = parseFloat(document.getElementById('nota2').value)
-    let media = (nota1 + nota2) / 2
+  let nome = document.getElementById('txtnome').value;
+  let nota1 = parseFloat(document.getElementById('nota1').value);
+  let nota2 = parseFloat(document.getElementById('nota2').value);
+  let media = (nota1 + nota2) / 2;
 
-    let msg
-    if (media >= 5) {
-        msg = 'Aprovado!'
-    } else {
-        msg = 'Reprovado!'
-    }
+  let msg;
 
-    res.innerHTML = `Calculando a média final de <strong>${nome}</strong> <br>
+  if (media >= 5) {
+    msg = 'Aprovado!';
+  } else {
+    msg = 'Reprovado!';
+  }
+
+  res.innerHTML = `Calculando a média final de <strong>${nome}</strong> <br>
     As notas obtidas foram <strong>${nota1}</strong> e <strong>${nota2}</strong> <br>
     A média final será <strong>${media}</strong> <br>
-    A situação final do aluno é: <strong>${msg}</strong>`
+    A situação final do aluno é: <strong>${msg}</strong>`;
 }
